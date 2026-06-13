46732c24ffff37b672310e733038b93627398d44
Chengqian_Wang <chengqiandaily@outlook.com>
2026-06-13 23:22:08 +0100

    update

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 import asyncio
-from dotenv import load_dotenv
 import os
+
+from dotenv import load_dotenv
+from langgraph.prebuilt import create_react_agent
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langchain_openai import ChatOpenAI
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
+
 load_dotenv()
-print(os.getenv("OPENAI_API_KEY"))
+
+llm = ChatOpenAI()
+
+stdio_server_params = StdioServerParameters(
+    command="python",
+    args=[r"C:\Users\Chengqian\Desktop\mcp-crash-course\servers\math_server.py"]
+)
+
 
 async def main():
     print("Hello from mcp-crash-course!")
